bfef6f069652d6eefb4519ac311c5a7b58edf44e
wangshuyi111 <2352291732@qq.com>
2026-07-11 22:48:54 +0800

    学习了Java中的关键字以及字面量和变量

---
+++ src/Exercise01.java
@@ -0,0 +1,12 @@
+//class：表示定义一个类，类是Java项目中最基本的组成单元
+//public：表示当前类是可访问的
+//Exercise01：类名，类名必须与文件名一致
+//{}：表示类的范围，所有的代码都需要写在大括号内
+public class Exercise01 {
+    //表示Java程序的主入口，当程序开始运作的时候，会从主入口开始逐行往下执行
+    //格式固定
+    public static void main(String[] args) {
+        //输出/打印语句，将里面的内容输出到控制台
+        System.out.println("Hello World!");
+    }
+}
