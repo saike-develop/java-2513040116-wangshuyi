a6fad6ce6f7cf18bccae417b79b53f86362c338c
wangshuyi111 <2352291732@qq.com>
2026-07-11 22:48:54 +0800

    学习了Java中的关键字以及字面量和变量

---
+++ src/Exercise02.java
@@ -0,0 +1,14 @@
+public class Exercise02 {
+    public static void main(String[] args) {
+        //输出我的信息：王姝懿 女 18 166
+        //输出我的名字
+        System.out.println("王姝懿");
+        //输出性别
+        System.out.println('女');
+        //输出年龄
+        System.out.println(18);
+        //输出身高
+        System.out.println(166);
+    }
+}
+
